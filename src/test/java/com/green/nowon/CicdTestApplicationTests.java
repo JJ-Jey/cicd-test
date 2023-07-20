fc4c750f21702e9a116ef285c6439f4d635d2157
Administrator <Administrator@304-14>
2023-07-20 09:48:39 +0900

    test01

---
+++ src/test/java/com/green/nowon/CicdTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.green.nowon;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
